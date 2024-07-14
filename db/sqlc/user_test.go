73d496bfff5d7f35e371541504cb68abb12695ca
arkarsg <alxarkar@gmail.com>
2024-07-14 23:32:15 +0800

    Complete tests for get user

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -22,8 +22,8 @@ func TestCreateUser(t *testing.T) {
 	user, err := createRandomAccount(test_input)
 	require.NoError(t, err)
 	require.NotEmpty(t, user)
-	require.Equal(t, test_input.Username, user.Username)
-	require.Equal(t, test_input.Email, user.Email)
+	assert.Equal(t, test_input.Username, user.Username)
+	assert.Equal(t, test_input.Email, user.Email)
 }
 
 func TestGetUserById(t *testing.T) {
@@ -40,4 +40,44 @@ func TestGetUserById(t *testing.T) {
 
 	require.NoError(t, err)
 	assert.Equal(t, expectedUser, actualUser)
+}
+
+func TestGetUserByUsername(t *testing.T) {
+	test_input := CreateUserParams{
+		Username: u.RandomUser(),
+		Email:    u.RandomEmail(),
+	}
+
+	expectedUser, _ := createRandomAccount(test_input)
+	actualUser, err := testQueries.GetUserByUsername(
+		context.Background(),
+		expectedUser.Username,
+	)
+
+	require.NoError(t, err)
+	assert.Equal(t, expectedUser, actualUser)
+}
+
+func TestListUsers(t *testing.T) {
+	test_input := ListUsersParams{
+		Limit:  2,
+		Offset: 0,
+	}
+	users, err := testQueries.ListUsers(context.Background(), test_input)
+	require.NoError(t, err)
+	require.Len(t, users, 2)
+}
+
+func TestDeleteUser(t *testing.T) {
+	testUserParams := CreateUserParams{
+		Username: u.RandomUser(),
+		Email:    u.RandomEmail(),
+	}
+	testUser, _ := createRandomAccount(testUserParams)
+	err := testQueries.DeleteUser(context.Background(), testUser.ID)
+	require.NoError(t, err)
+
+	user, err := testQueries.GetUserById(context.Background(), testUser.ID)
+	require.Error(t, err)
+	assert.Empty(t, user)
 }
